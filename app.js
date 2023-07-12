7bb9acd5d11c1c0008423a9a2a62af22502c47e7
sonamnayak <sonamnayak2710@gmail.com>
2023-07-12 13:26:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.body.classList.toggle("open");
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  document.body.classList.toggle("open");
+});
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    document.querySelector(".header_container").classList.add("on_scroll");
+  else
+    document.querySelector(".header_container").classList.remove("on_scroll");
+};
+
+const scroll = document.getElementById("scroll");
+const hero = document.querySelector(".hero");
+
+const isContainerAtTop = () => {
+  const containerRect = hero.getBoundingClientRect();
+  return containerRect.top < -50;
+};
+window.addEventListener("scroll", () => {
+  if (isContainerAtTop()) {
+    scroll.classList.add("active");
+  } else {
+    scroll.classList.remove("active");
+  }
+});
+
+Array.from(document.querySelectorAll(".nav_li")).forEach((ele) =>
+  ele.addEventListener("click", () => document.body.classList.toggle("open"))
+);
+
+for(let i = 5; i>=0; i--) {
+    console.log(n)
+}
+
+const func = (n) => {
+    if(n===0) return
+    console.log(n)
+    func(n-1)
+}
